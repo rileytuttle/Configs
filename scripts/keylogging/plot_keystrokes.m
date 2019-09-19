191d69c39f2a9df8fda0679b4d509b20ce76c6ee
Riley Tuttle <rtuttle@irobot.com>
2019-09-19 12:13:28 -0400

    adding plotting function to keylogcounter

--- scripts/keylogging/plot_keystrokes.m
+++ scripts/keylogging/plot_keystrokes.m
@@ -1,10 +1,17 @@
-fid = fopen('keystrokedata.csv')
-a = textscan(fid, '%s %u');
-fclose(fid);
-dates = zeros(size(a{1}));
-for i = 1:length(a{1})
-    dates(i,1) = datenum(a{1}{i}, 29);
+function plot_keystrokes(varargin)
+    if nargin == 1
+        fid = fopen(varargin{1})
+        a = textscan(fid, '%s %u');
+        fclose(fid);
+        dates = zeros(size(a{1}));
+        for i = 1:length(a{1})
+            dates(i,1) = datenum(a{1}{i}, 29);
+        end
+        plot(dates,a{2}, '.b')
+        datetick('x', 'yyyy-mm-dd')
+        print -djpg plot.jpg
+    else
+        disp('too many args')
+    end
+        
 end
-plot(dates,a{2}, '.b')
-datetick('x', 'yyyy-mm-dd')
-    
