951fdf3a1270f06a5d6f54f70adf95d56c8b88a2
Riley Tuttle <rileytuttle@gmail.com>
2025-06-12 09:50:30 -0400

    splitting out setup functions so I can use them in work setup as well

--- setup-my-bash.sh
+++ setup-my-bash.sh
@@ -2,22 +2,12 @@
 #
 # WORK IN PROGRESS
 
-if [ ! -d "$HOME/.oh-my-bash" ]; then
-    echo "installing oh my bash"
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-    exit 0
-fi
+source bash-setup-functions
 
-echo 'if [ -f ~/Configs/bash-scripts/git-script.sh ]; then' >> ~/.bashrc
-echo '    source ~/Configs/bash-scripts/git-scripts.sh' >> ~/.bashrc
-echo 'fi' >> ~/.bashrc
+install_oh_my_bash
 
-if ! command -v rg >/dev/null; then
-    sudo apt install ripgrep
-fi
+add_source_git_scripts
 
-if ! command -v fzf >/dev/null; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-    echo 'export FZF_DEFAULT_OPTS="--layout=reverse"' >> ~/.bashrc
-fi
+install_ripgrep
+
+install_fzf
