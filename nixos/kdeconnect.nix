1069f07d93dbe93030cc994ab74178e960b3357f
Riley Tuttle <rileytuttle@gmail.com>
2026-04-05 22:00:03 -0400

    adding kdeconnect

---
+++ nixos/kdeconnect.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = [ pkgs.kdePackages.kdeconnect-kde ];
+
+  networking.firewall = {
+    allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
+    allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
+  };
+}
