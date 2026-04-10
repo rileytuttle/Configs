97f57ad0527818b35c7fff52ebb73038f884e008
Riley Tuttle <rileytuttle@gmail.com>
2026-04-10 12:07:52 -0400

    adding night light stuff but I am not sure it works

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -96,5 +96,12 @@
       enable = true;
       defaultOptions = [ "--layout=reverse" ];
     };
-};
+
+    dconf.settings = {
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-enabled = true;
+        night-light-schedule-automatic = true;  # use manual times
+        night-light-temperature = 4000;    # color temp (1000-6500, lower = warmer)
+      };
+    };
 }
