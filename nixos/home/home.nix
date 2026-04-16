cc4e0f5a742ae345e9e480910ae2e7ae0a45c947
Riley Tuttle <rileytuttle@gmail.com>
2026-04-15 21:24:45 -0400

    adding bitwarden fzf defaults and kde plasma desktop environment option

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -21,6 +21,7 @@
         xournalpp
         ripgrep
         slack
+        bitwarden-desktop
     ]; 
     home.file.".config/kak/kakrc".source = /home/rileytuttle/Configs/dotfiles/kakrc/kakrc;
     home.file.".tmux.conf".source = /home/rileytuttle/Configs/dotfiles/.tmux.conf;
@@ -58,6 +59,7 @@
         export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[33m\]$(parse_git_branch)\[\033[00m\]\$ '
 
         # fzf keybindings if you have it installed
+        export FZF_CTRL_R_OPTS="--layout=reverse"
         [ -f ~/.fzf.bash ] && source ~/.fzf.bash
         eval "$(${pkgs.fzf}/bin/fzf --bash)"
         # bind up arrow to search history by partial command
