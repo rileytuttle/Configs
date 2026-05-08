dd13419dc2aa81aa3d507d329919d5642df931f2
Riley Tuttle <rileytuttle@gmail.com>
2026-05-07 22:51:50 -0400

    adding transmission

--- nixos/fw12/configuration.nix
+++ nixos/fw12/configuration.nix
@@ -187,4 +187,20 @@ in
     dedicatedServer.openFirewall = true;
   };
 
+  services.transmission = {
+    enable = true;
+    openFirewall = true; # Opens port 51413 for peer-to-peer
+    openRPCPort = true;  # Opens port 9091 for web interface
+    user = "rileytuttle";
+    settings = {
+      download-dir = "/home/rileytuttle/Downloads";
+      incomplete-dir = "/home/rileytuttle/.transmission-incomplete";
+      incomplete-dir-enabled = true;
+      rpc-enabled = true;
+      rpc-bind-address = "127.0.0.1";   # or 127.0.0.1 if local only
+      rpc-whitelist-enabled = true;
+      rpc-whitelist = "127.0.0.1,::1";
+    };
+  };
+  
 }
